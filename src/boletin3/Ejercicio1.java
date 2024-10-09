c27a9640feb4a9006e63056db5fff17f4d3ec78d
diego.fernandez <diego.fernandez@106-10.info2>
2024-10-09 10:13:45 +0200

    Boletin3 Ejercicio 1 empezado

---
+++ src/boletin3/Ejercicio1.java
@@ -0,0 +1,24 @@
+package boletin3;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		
+		//
+		int num;
+		int cifra1;
+		int cifra2;
+		int cifra3;
+		int cifra4;
+		
+		//Creo el escaner para leer lo que pone el usuario.
+		Scanner  sc = new Scanner (System.in);
+		
+		//
+		System.out.println("Introduzca el numero capicua");
+		num=sc.nextInt();
+		
+
+}
+}
