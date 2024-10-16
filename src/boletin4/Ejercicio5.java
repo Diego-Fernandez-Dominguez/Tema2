1fb1cad78a2a4cc02209c51b755546fa99643486
usuario <usuario@192.168.1.139>
2024-10-16 20:50:27 +0200

    Comentado y mejora ejercicios

--- src/boletin4/Ejercicio5.java
+++ src/boletin4/Ejercicio5.java
@@ -10,7 +10,7 @@ public class Ejercicio5 {
 
 		// Creo la variable que va a guardar la suma de los numeros
 		int suma = 0;
-				
+
 		// Creo la variable que va a guardar la cantidad de numeros introducidos
 		int contador = 0;
 		int contMayor = 0;
@@ -25,26 +25,37 @@ public class Ejercicio5 {
 		// Hasta que num no sea negativo, se va a repetir este bucle
 		while (edad >= 0) {
 
-			if (edad >= 18){
+			// Si la edad es mayor o igual a 18, le sumo 1 al contador de mayores de edad
+			if (edad >= 18) {
 				contMayor++;
 			}
-			
-			suma=suma+edad;
-			
+
+			// Hago la suma
+			suma = suma + edad;
+
 			// Le sumo 1 al contador
 			contador++;
+			
 			// Vuelvo a pedir el numero al usuario
 			System.out.println("\nIntroduzca la edad de un alumno");
 			edad = sc.nextInt();
 
 		}
 
-		// Si llega aqui, el usuario ha puesto un numero acabado. Saco la cantidad por pantalla
-		System.out.println("La suma de las edades es: " + suma );
+		// Si llega aqui, el usuario ha puesto el numero de acabado. Saco la suma por
+		// pantalla
+		System.out.println("La suma de las edades es: " + suma);
+
+		// Hago la media y la saco por pantalla
 		System.out.println("La media de las edades es: " + (double) suma / contador);
-		System.out.println("Has introducido " + contador + " alumnos" );
-		System.out.println(contMayor==0 ? "No se han introducido ningun alumno mayor de edad"
-				:"Has introducido " + contMayor + " alumnos mayores de edad" );
+
+		// Saco el contador de alumnos por pantalla
+		System.out.println("Has introducido " + contador + " alumnos");
+
+		// Si el contador de mayores de edad es igual a 0, el prorama dirá que no se ha
+		// introducido ningun mayor de edad
+		System.out.println(contMayor == 0 ? "No se han introducido ningun alumno mayor de edad"
+				: "Has introducido " + contMayor + " alumnos mayores de edad");
 
 		// Cierro el escaner
 		sc.close();
--- src/english/Exercise4.java
+++ src/english/Exercise4.java
@@ -7,39 +7,44 @@ public class Exercise4 {
 	public static void main(String[] args) {
 
 		// I create the variable that is going to save the number introduced by the user
-		int num=0;
-		
+		int num = 0;
+
+		// I create the variable that is going to save the secret random number
 		int secretNumber;
-		
+
 		// I create the scanner to read the numbers introduced by the user
 		Scanner sc = new Scanner(System.in);
-		
+
+		// I create the random to create the secret number
 		Random rand = new Random();
 
+		// The rand.nextInt generates the secret number, that is going to be between 1 and 100
+		secretNumber = rand.nextInt(1, 101);
 
-		System.out.println("\nIntroduce a number");
-		num=sc.nextInt();
-		secretNumber= rand.nextInt(1,101);
+		// Until the number is different to -1 or the secret number, is going to repeat
+		// the next loop
+		while (num != -1 && num != secretNumber) {
 
-		// Until the counter is less or equal than 10, is going to repeat the next loop
-		while (num != -1 || num==secretNumber) {
+			// Ask the user to introduce a number
+			System.out.println("\nIntroduce a number");
+			num = sc.nextInt();
 
-			
-			if (num==secretNumber) {
+			// If the users number is equal to the secret number, it means that the user won
+			if (num == secretNumber) {
 				System.out.println("\nCongratulations, you guessed it =-) ");
-				
-			} else if(num>secretNumber) {
+
+				// If the number is higher than the secret number, the program says it
+			} else if (num > secretNumber) {
 				System.out.println("\nYour number is higher than the secret number");
-				
+
+				// If the number is higher than the secret number, the program says it
 			} else {
 				System.out.println("\nYour number is lower than the secret number");
 			}
 
-			System.out.println("\nIntroduce a number");
-			num=sc.nextInt();
-						
 		}
 
+		// I print that the game has ended
 		System.out.println("Game ended");
 
 		// Close the scanner
